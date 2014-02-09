356088a27dd4be6c60505561a0ea0677c737f4b3
kallaballa <amir@viel-zu.org>
2014-02-09 04:40:37 +0100

    put settings and triggerbase into their own compilation untis

--- janosh.hpp
+++ janosh.hpp
@@ -26,7 +26,8 @@
 #include <boost/filesystem.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <kcpolydb.h>
-
+#include "trigger_base.hpp"
+#include "settings.hpp"
 #include "logger.hpp"
 #include "record.hpp"
 #include "json_spirit/json_spirit.h"
@@ -47,50 +48,6 @@
   using std::ifstream;
   using std::exception;
 namespace janosh {
-  class TriggerBase {
-    typedef std::map<string,string> TargetMap;
-    typedef typename TargetMap::value_type Target;
-
-    vector<fs::path> targetDirs;
-    map<Path, std::set<string> > triggers;
-    TargetMap targets;
-public:
-    TriggerBase(const fs::path& config, const vector<fs::path>& targetDirs);
-
-    int executeTarget(const string& name, std::ostream& out);
-    void executeTrigger(const Path& p, std::ostream& out);
-
-    bool findAbsoluteCommand(const string& cmd, string& abs);
-    void load(const fs::path& config);
-    void load(std::ifstream& is);
-    template<typename T> void error(const string& msg, T t, int exitcode=1) {
-      LOG_ERR_MSG(msg, t);
-      exit(exitcode);
-    }
-
-    template<typename T> void warn(const string& msg, T t) {
-      LOG_WARN_MSG(msg, t);
-    }
-  };
-
-  class Settings {
-  public:
-    fs::path janoshFile;
-    fs::path databaseFile;
-    fs::path triggerFile;
-    fs::path logFile;
-    int port;
-    vector<fs::path> triggerDirs;
-
-    Settings();
-    template<typename T> void error(const string& msg, T t, int exitcode=1) {
-      LOG_ERR_MSG(msg, t);
-      exit(exitcode);
-    }
-  private:
-    bool find(const js::Object& obj, const string& name, js::Value& value);
-  };
-
   class Command;
   typedef map<const std::string, Command*> CommandMap;
 
