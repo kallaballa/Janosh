52dd51d0950d81223c63e2113890099e674c9ec9
kallaballa <amir@viel-zu.org>
2014-02-13 18:26:58 +0100

    close janosh on sigint

--- janosh.cpp
+++ janosh.cpp
@@ -882,8 +882,10 @@ void handleSigInt(int s) {
     TcpServer::getInstance()->close();
   }
 
-  LOG_DEBUG_MSG("Shutting down janosh due to sigint", s);
-  Janosh::getInstance()->close();
+  if(Janosh::getInstance()->isOpen()) {
+    LOG_DEBUG_MSG("Shutting down janosh server due to sigint", s);
+    Janosh::getInstance()->close();
+  }
 }
 
 void registerSigIntHandler() {
