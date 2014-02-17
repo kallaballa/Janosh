f900155c002412d5483a114a996b05386f0f094f
kallaballa <amir@viel-zu.org>
2014-02-17 15:52:03 +0100

    escape ticks

--- bash.cpp
+++ bash.cpp
@@ -17,7 +17,9 @@ void BashPrintVisitor::close() {
 void BashPrintVisitor::record(const Path& p, const string& value, bool array, bool first) {
   string stripped = value;
   replace(stripped.begin(), stripped.end(), '\n', ' ');
-  boost::algorithm::replace_all(stripped, "&", "&#39;");
+  boost::algorithm::replace_all(stripped, "&", "&#38;");
+  boost::algorithm::replace_all(stripped, "'", "&#39;");
+
   out << '[' << p.pretty() << "]='" << stripped << "' ";
 }
 }
