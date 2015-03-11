4858e9d987e10b30b1604ed4648887f388b31efe
kallaballa <amir@viel-zu.org>
2015-03-11 17:42:20 +0100

    formatting

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -13,7 +13,8 @@ namespace janosh {
 
 using boost::asio::ip::tcp;
 
-TcpClient::TcpClient() : socket(io_service) {
+TcpClient::TcpClient() :
+    socket(io_service) {
 }
 
 TcpClient::~TcpClient() {
@@ -21,10 +22,10 @@ TcpClient::~TcpClient() {
 }
 
 void TcpClient::connect(string host, int port) {
-	    tcp::resolver resolver(io_service);
-	    tcp::resolver::query query(host, std::to_string(port));
-	    tcp::resolver::iterator iterator = resolver.resolve(query);
-	    boost::asio::connect(socket, iterator);
+  tcp::resolver resolver(io_service);
+  tcp::resolver::query query(host, std::to_string(port));
+  tcp::resolver::iterator iterator = resolver.resolve(query);
+  boost::asio::connect(socket, iterator);
 }
 
 int TcpClient::run(Request& req, std::ostream& out) {
@@ -34,35 +35,34 @@ int TcpClient::run(Request& req, std::ostream& out) {
   write_request(req, request_stream);
 
   boost::asio::write(socket, request);
-	boost::asio::streambuf response;
-	std::istream response_stream(&response);
-  boost::asio::read_until(socket, response,"\n");
+  boost::asio::streambuf response;
+  std::istream response_stream(&response);
+  boost::asio::read_until(socket, response, "\n");
 
-	string strReturnCode;
-	std::getline(response_stream, strReturnCode);
-	int returnCode = std::stoi(strReturnCode);
+  string strReturnCode;
+  std::getline(response_stream, strReturnCode);
+  int returnCode = std::stoi(strReturnCode);
 
-	if(returnCode == 0 ) {
-	  LOG_DEBUG_STR("Successful");
-	}
-	else {
-	  LOG_INFO_STR("Failed");
-	}
+  if (returnCode == 0) {
+    LOG_DEBUG_STR("Successful");
+  } else {
+    LOG_INFO_STR("Failed");
+  }
 
-	try {
-	  string line;
-	  while(response_stream) {
-	    boost::asio::read_until(socket, response,"\n");
-	    std::getline(response_stream, line);
-	    out << line << std::endl;
-	  }
-	} catch(std::exception& ex) {
-	}
-	return returnCode;
+  try {
+    string line;
+    while (response_stream) {
+      boost::asio::read_until(socket, response, "\n");
+      std::getline(response_stream, line);
+      out << line << std::endl;
+    }
+  } catch (std::exception& ex) {
+  }
+  return returnCode;
 }
 
 void TcpClient::close() {
-	socket.close();
+  socket.close();
 }
 
 } /* namespace janosh */
