11365da83580aa6e931265c65ff21511a4165265
kallaballa <amir@viel-zu.org>
2015-03-17 15:50:44 +0000

    fix build_dependencies for arm

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -10,4 +10,5 @@ make install
 echo "Building zeromq lua bindings..."
 LIBDIR=/usr/lib
 test -d /usr/lib64 && LIBDIR=/usr/lib64
+test -d /usr/lib/arm-linux-gnueabihf && LIBDIR=/usr/lib/arm-linux-gnueabihf
 luarocks install https://raw.github.com/Neopallium/lua-zmq/master/rockspecs/lua-zmq-scm-1.rockspec ZEROMQ_DIR=/usr/ ZEROMQ_LIBDIR="$LIBDIR"
