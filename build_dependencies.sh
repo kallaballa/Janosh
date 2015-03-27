22067985aeddb6e6da233c56d1e9a1c817630e7f
kallaballa <amir@viel-zu.org>
2015-03-27 12:17:51 +0100

    build deps for debian amd64

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -11,5 +11,7 @@ echo "Building zeromq lua bindings..."
 LIBDIR=/usr/lib
 test -d /usr/lib64 && LIBDIR=/usr/lib64
 test -d /usr/lib/arm-linux-gnueabihf && LIBDIR=/usr/lib/arm-linux-gnueabihf
+test -d /usr/lib/x86_64-linux-gnu/ && LIBDIR=/usr/lib/x86_64-linux-gnu/
+
 export PATH="$PATH:/usr/local/bin/"
 luarocks install https://raw.github.com/Neopallium/lua-zmq/master/rockspecs/lua-zmq-scm-1.rockspec ZEROMQ_DIR=/usr/ ZEROMQ_LIBDIR="$LIBDIR"
