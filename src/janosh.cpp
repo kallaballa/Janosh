ce6c3433eb851feb61ddf8738b808816bf2e0327
kallaballa <amir@viel-zu.org>
2018-01-06 16:12:28 +0100

    remove easylogging init macros

--- src/janosh.cpp
+++ src/janosh.cpp
@@ -1101,8 +1101,6 @@ using namespace boost;
 using namespace janosh;
 namespace po = boost::program_options;
 
-INITIALIZE_EASYLOGGINGPP
-
 int main(int argc, char** argv) {
   START_EASYLOGGINGPP(0, (const char**)NULL);
   try {
