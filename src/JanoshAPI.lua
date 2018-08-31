850d10de0db1a4681476d2b7577b20ac4063817a
kallaballa <amir@viel-zu.org>
2018-08-31 03:31:54 +0200

    base64 builtins

--- src/JanoshAPI.lua
+++ src/JanoshAPI.lua
@@ -78,6 +78,37 @@ function JanoshClass.fclose(self, fd)
   posix.close(fd)
 end
 
+local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' -- You will need this for encoding/decoding
+-- encoding
+function JanoshClass.enc64(self,data)
+    return ((data:gsub('.', function(x) 
+        local r,b='',x:byte()
+        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c=0
+        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+        return b:sub(c+1,c+1)
+    end)..({ '', '==', '=' })[#data%3+1])
+end
+
+-- decoding
+function JanoshClass.dec64(self, data)
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+            return string.char(c)
+    end))
+end
+
 function JanoshClass.tprint(self, tbl, indent)
   if not indent then indent = 0 end
   for k, v in pairs(tbl) do
@@ -505,6 +536,10 @@ end
 
 function JanoshClass.shift_t(self, from, to)
   janosh_request_t({"shift", from,to})
+end
+
+function JanoshClass.move(self, from, to)
+  janosh_request({"move", from,to})
 end
 
 function JanoshClass.move_t(self, from, to)
