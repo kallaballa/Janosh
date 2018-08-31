4ff61252926bb12d34cb537f16cf1af50288e5ad
kallaballa <amir@viel-zu.org>
2018-08-31 00:04:56 -0400

    updated build_dependencies

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,10 +1,12 @@
 
+sudo id
 echo "Building luajit and luarocks..."
+rm -fr luajit-rocks/
 git clone https://github.com/torch/luajit-rocks.git
-cd luajit-rocks/lua-5.1
+cd luajit-rocks/luajit-2.0
 mkdir build
 cd build
 make clean
 cmake ..
-make install
+sudo make install
 
