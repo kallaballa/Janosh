958c09faf7d6daad100349cd72f52fcc1e259c36
kallaballa <amir@viel-zu.org>
2018-09-25 13:00:50 +0200

    remove socket options

--- src/tcp_client.cpp
+++ src/tcp_client.cpp
@@ -29,10 +29,7 @@ void TcpClient::connect(string host, int port) {
   if(sock_ != NULL)
     delete sock_;
   sock_ = new zmq::socket_t(context_, ZMQ_REQ);
-  sock_->setsockopt(ZMQ_IMMEDIATE, 1);
-  sock_->setsockopt(ZMQ_RECONNECT_IVL, -1);
   sock_->connect(("tcp://" + host + ":" + std::to_string(port)).c_str());
-  std::cerr << sock_->connected() << std::endl;
 }
 
 bool endsWith(const std::string &mainStr, const std::string &toMatch)
