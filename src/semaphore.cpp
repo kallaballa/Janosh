8e41dae4ef5e06c4e820ce6393591134e9d7393c
kallaballa <amir@viel-zu.org>
2018-09-26 20:09:06 +0200

    switched tu uWebsocket

---
+++ src/semaphore.cpp
@@ -0,0 +1,27 @@
+#include <mutex>
+#include <condition_variable>
+#include "semaphore.hpp"
+
+namespace janosh {
+void Semaphore::notify() {
+  std::lock_guard<decltype(mutex_)> lock(mutex_);
+  ++count_;
+  condition_.notify_one();
+}
+
+void Semaphore::wait() {
+  std::unique_lock<decltype(mutex_)> lock(mutex_);
+  while (!count_) // Handle spurious wake-ups.
+    condition_.wait(lock);
+  --count_;
+}
+
+bool Semaphore::try_wait() {
+  std::lock_guard<decltype(mutex_)> lock(mutex_);
+  if (count_) {
+    --count_;
+    return true;
+  }
+  return false;
+}
+}
---
+++ src/semaphore.hpp
@@ -0,0 +1,23 @@
+#ifndef SRC_SEMAPHORE_HPP_
+#define SRC_SEMAPHORE_HPP_
+
+#include <mutex>
+#include <condition_variable>
+
+namespace janosh {
+class Semaphore
+{
+private:
+    std::mutex mutex_;
+    std::condition_variable condition_;
+    unsigned long count_; // Initialized as locked.
+
+public:
+    Semaphore(unsigned long count) : count_(count) {}
+    void notify();
+    void wait();
+    bool try_wait();
+};
+}
+
+#endif /* SRC_SEMAPHORE_HPP_ */
