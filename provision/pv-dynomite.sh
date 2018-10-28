d5116e421f01c8200b02f29e83c2ce628aebf838
kallaballa <amir@viel-zu.org>
2018-10-28 15:49:47 +0100

    new provision script for dynomite

---
+++ provision/pv-dynomite.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -x
+set -e
+
+CORES="$1"
+
+apt-get -y install dh-autoreconf libssl-dev
+
+git clone https://github.com/kallaballa/dynomite.git
+cd dynomite
+autoreconf -fvi
+./configure
+make -j$CORES
+make install
+cd ..
+
+
+mkdir -p /etc/dynomite/
+cat > /etc/dynomite/dynomite.conf <<EODYNOCONF
+dyn_o_mite:
+  dyn_listen: 0.0.0.0:8101
+  data_store: 2
+  listen: 0.0.0.0:8102
+  dyn_seed_provider: simple_provider
+  servers:
+  - 127.0.0.1:1978:1
+  tokens: 437425602
+  stats_listen: 0.0.0.0:22333
+EODYNOCONF
+
+cat > /etc/systemd/system/dynomite.service <<EODYNOS
+[Unit]
+Description=Dynomite Daemon
+
+[Service]
+WorkingDirectory=/home/janosh
+ExecStart=/usr/local/sbin/dynomite -c /etc/dynomite/dynomite.conf
+User=janosh
+Group=janosh
+
+[Install]
+WantedBy=multi-user.target
+
+EODYNOS
+
+chmod 664 /etc/systemd/system/dynomite.service
+systemctl daemon-reload
+systemctl enable dynomite
+systemctl start dynomite
+
